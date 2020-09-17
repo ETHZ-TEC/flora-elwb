564cc2334305721803770565008f05da45180430
Reto Da Forno <reto.daforno@tik.ee.ethz.ch>
2020-09-17 12:00:32 +0200

    version number bumped to 0.1.19

--- Inc/app_config.h
+++ Inc/app_config.h
@@ -19,7 +19,7 @@
 #define FW_NAME                         "DPP2eLWB"  /* max. 8 chars */
 #define FW_VERSION_MAJOR                0           /* 0..6 */
 #define FW_VERSION_MINOR                1           /* 0..99 */
-#define FW_VERSION_PATCH                18          /* 0..99 */
+#define FW_VERSION_PATCH                19          /* 0..99 */
 
 #define FLOCKLAB                        0           /* set to 1 to run on FlockLab */
 #define BASEBOARD                       0           /* set to 1 if the comboard will be installed on a baseboard */
